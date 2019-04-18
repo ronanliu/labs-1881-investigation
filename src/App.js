b761bfaee5d183eb06c10095fffb585f599baa88
Ronan Liu <ronan.liu@iress.com>
2019-04-18 16:47:00 +1000

    Add modal to handle content to simulate the design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { Button, Modal } from "antd";
 import "antd/dist/antd.css";
 import React, { Component } from "react";
 import List from "./List";
 
 class App extends Component {
+  state = {
+    visible: false
+  };
+
+  handleBtnClick = () => this.setState({ visible: true });
+  handleModalClose = () => this.setState({ visible: false });
+
   render() {
     return (
-      <div>
-        <List />
+      <div style={{ margin: 50 }}>
+        <Button onClick={this.handleBtnClick}>Open Modal</Button>
+        <Modal
+          visible={this.state.visible}
+          onOk={this.handleModalClose}
+          onCancel={this.handleModalClose}
+          title="Client dashboard settings"
+        >
+          <List />
+        </Modal>
       </div>
     );
   }
